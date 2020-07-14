1225f7634105658a9f2994d22ff8049f9d3486e4
juanixco0192 <ixcj8573@chsphantoms.org>
2020-07-14 17:23:09 +0000

    code

--- src/main/java/com/team4099/falconcamps/subsystems/ShooterSparkPID.java
+++ src/main/java/com/team4099/falconcamps/subsystems/ShooterSparkPID.java
@@ -1,6 +1,8 @@
 package com.team4099.falconcamps.subsystems;
 
 import com.revrobotics.*;
+
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSparkPID extends SubsystemBase {
@@ -9,7 +11,19 @@ public class ShooterSparkPID extends SubsystemBase {
     private final CANEncoder shooterEncoder = shooterLeader.getEncoder();
     private final CANPIDController pidController = shooterFollower.getPIDController();
 
-    public ShooterSparkPID() {}
+    public ShooterSparkPID() {
+        super(new PIDController(1.7, 300, 0.0, 5.5 * 14000));
+    }
+    public double getCurrentVelocity() {
+        return shooterEncoder.getVelocity();
+    }
+    
+    public double getMeasurement() {
+        return getCurrentVelocity();
+    }
+    public void useOutput(double setPoint,double output) {
+        shooterLeader.set(output);
+    }
 
     @Override
     public void periodic() {}
